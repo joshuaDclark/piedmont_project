3620bb615d9b5e367558f71dc5f8fad5aab99a95
joshuaDclark <mrjoshuadclark@gmail.com>
2018-10-03 21:34:29 -0400

    made migrations, created superuser, started questionaire app

--- src/piedmontpuppies_project/piedmontpuppies_project/settings.py
+++ src/piedmontpuppies_project/piedmontpuppies_project/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'piedmontpuppies_project',
+
+    # third party
+
+    #own
+    'questionaire',
+
 ]
 
 MIDDLEWARE = [
