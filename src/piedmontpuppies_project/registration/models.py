66fe693010ee4257f841d863bb6f7c2ceec87395
joshuaDclark <mrjoshuadclark@gmail.com>
2019-07-26 20:40:49 -0400

    second commit

--- src/piedmontpuppies_project/registration/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Contact(models.Model):
-    first_name = models.CharField(max_length=50, null=True, blank=False)
-    last_name = models.CharField(max_length=50, null=True, blank=False)
-    phone_number = models.CharField(max_length=10, null=True, blank=False)
-    email = models.EmailField(max_length=40, null=True, blank=False)
